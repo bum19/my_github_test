3d34875369f805b3f6114e247d9cea6316f29fc3
bum19 <1996joon@gmail.com>
2022-12-16 14:09:54 +0900

    initial spring boot setting commmit

---
+++ src/main/java/com/todoSample/MyGithubTestApplication.java
@@ -0,0 +1,13 @@
+package com.todoSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGithubTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoSample/MyGithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGithubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
